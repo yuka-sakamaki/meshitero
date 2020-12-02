9c73cd93f0fb6778c8f220db34862e92ecc4cfcd
super-sausage <69895997+super-sausage@users.noreply.github.com>
2020-12-02 12:04:14 +0900

    first commit

---
+++ app/forms/tweets_tag.rb
@@ -0,0 +1,20 @@
+class TweetsTag
+
+  include ActiveModel::Model
+  attr_accessor :message, :name, :image
+
+  with_options presence: true do
+    validates :message
+    validates :name
+    validates :image
+  end
+
+  def save
+    tweet = Tweet.create(message: message, image: image)
+    tag = Tag.where(name: name).first_or_initialize
+    tag.save
+
+    TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
+  end
+
+end
---
+++ app/models/tag.rb
@@ -0,0 +1,7 @@
+class Tag < ApplicationRecord
+
+  has_many :tweet_tag_relations
+  has_many :tweets, through: :tweet_tag_relations
+
+  validates :name, uniqueness: true
+end
